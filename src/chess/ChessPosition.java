70d07a07eb2d52476d93e2441b4483db7f95de76
Lucas Kellmer <lucaskellmer@hotmail.com>
2022-11-28 10:54:01 -0300

    Check logic

--- src/chess/ChessPosition.java
+++ src/chess/ChessPosition.java
@@ -18,15 +18,19 @@ public class ChessPosition {
 	public char getColumn() {
 		return column;
 	}
+	
 	public int getRow() {
 		return row;
 	}
+	
 	protected Position toPosition() {
-		return new Position(8-row, column - 'a');
+		return new Position(8 - row, column - 'a');
 	}
+	
 	protected static ChessPosition fromPosition(Position position) {
-		return new ChessPosition((char)('a'-position.getColumn()),8-position.getRow());
+		return new ChessPosition((char)('a' + position.getColumn()),8 - position.getRow());
 	}
+	
 	@Override
 	public String toString() {
 		return "" + column + row;
